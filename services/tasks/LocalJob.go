6907fb4d92bcd43aaccf36a7c075da4f1befae98
Csaba Istvan Berta <9463439+fddn@users.noreply.github.com>
2023-12-29 14:46:42 +0000

    Revert "try token env var"
    This reverts commit e9c2a2b826715b4eee81207304cb697725c7e7d9.

--- services/tasks/LocalJob.go
+++ services/tasks/LocalJob.go
@@ -446,26 +446,19 @@ func (t *LocalJob) installCollectionsRequirements() error {
 	// requirementsFilePath := path.Join(t.getPlaybookDir(), "collections", "requirements.yml")
 	requirementsFilePath := fmt.Sprintf("%s/collections/requirements.yml", t.getRepoPath())
 	requirementsHashFilePath := fmt.Sprintf("%s.md5", requirementsFilePath)
-	token := os.Getenv("TOKEN")
 
 	if _, err := os.Stat(requirementsFilePath); err != nil {
 		t.Log("No collections/requirements.yml file found. Skip galaxy install process.\n")
 		return nil
 	}
 	
-	command := []string{
+	if err := t.runGalaxy([]string{
 		"collection",
 		"install",
 		"-r",
 		requirementsFilePath,
 		"--force",
-	}
-
-	if token != "" {
-		command = append(command, "--token", token)
-	}
-
-	if err := t.runGalaxy(command); err != nil {
+	}); err != nil {
 		return err
 	}
 	if err := writeMD5Hash(requirementsFilePath, requirementsHashFilePath); err != nil {
